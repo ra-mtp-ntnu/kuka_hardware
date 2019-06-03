8232bcc3b7895495b0db29d89021be785785b40f
Lars Tingelstad <lars.tingelstad@ntnu.no>
2019-06-03 22:32:51 +0200

    Moved most of the code

--- kuka_rsi_simulator/kuka_rsi_simulator/kuka_rsi_simulator.py
+++ kuka_rsi_simulator/kuka_rsi_simulator/kuka_rsi_simulator.py
@@ -12,27 +12,87 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+import socket
+import numpy as np
+import time
+import errno
+import xml.etree.ElementTree as ET
+
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
 
+node_name = 'kuka_rsi_simulator'
+
 
 class KukaRsiSimulator(Node):
 
     def __init__(self):
-        super().__init__('kuka_rsi_simulator')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
+        super().__init__(node_name)
+        self._publisher = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.004  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.settimeout(1)
+        self._host = '127.0.0.1'
+        self._port = 49152
+
+        self.act_joint_pos = np.array([0.0, -90.0, 90.0, 0.0, 90.0, 0.0])
+        self.cmd_joint_pos = self.act_joint_pos.copy()
+        self.des_joint_correction_absolute = np.zeros(6)
+        self.timeout_count = 0
+        self.ipoc = 0
+
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        try:
+            msg = self.create_rsi_xml_rob(
+                self.act_joint_pos, self.cmd_joint_pos, self.timeout_count, self.ipoc)
+            self._socket.sendto(msg, (self._host, self._port))
+            recv_msg, addr = self._socket.recvfrom(1024)
+            self.des_joint_correction_absolute, ipoc_recv = self.parse_rsi_xml_sen(
+                recv_msg)
+            self.act_joint_pos = self.cmd_joint_pos + self.des_joint_correction_absolute
+            self.ipoc += 1
+        except socket.timeout:
+            self.get_logger().warn('{}: Socket timed out'.format(node_name))
+            self.timeout_count += 1
+        # except socket.error, e:
+        #     if e.errno != errno.EINTR:
+        #         raise
+
+        pubmsg = String()
+        pubmsg.data = str(msg)[1:]
+        self._publisher.publish(pubmsg)
+        self.get_logger().info('Publishing: "%s"' % pubmsg.data)
         self.i += 1
+
+    def create_rsi_xml_rob(self, act_joint_pos, setpoint_joint_pos, timeout_count, ipoc):
+        q = act_joint_pos
+        qd = setpoint_joint_pos
+        root = ET.Element('Rob', {'TYPE': 'KUKA'})
+        ET.SubElement(root, 'RIst', {'X': '0.0', 'Y': '0.0', 'Z': '0.0',
+                                     'A': '0.0', 'B': '0.0', 'C': '0.0'})
+        ET.SubElement(root, 'RSol', {'X': '0.0', 'Y': '0.0', 'Z': '0.0',
+                                     'A': '0.0', 'B': '0.0', 'C': '0.0'})
+        ET.SubElement(root, 'AIPos', {'A1': str(q[0]), 'A2': str(q[1]), 'A3': str(q[2]),
+                                      'A4': str(q[3]), 'A5': str(q[4]), 'A6': str(q[5])})
+        ET.SubElement(root, 'ASPos', {'A1': str(qd[0]), 'A2': str(qd[1]), 'A3': str(qd[2]),
+                                      'A4': str(qd[3]), 'A5': str(qd[4]), 'A6': str(qd[5])})
+        ET.SubElement(root, 'Delay', {'D': str(timeout_count)})
+        ET.SubElement(root, 'IPOC').text = str(ipoc)
+        return ET.tostring(root)
+
+    def parse_rsi_xml_sen(self, data):
+        root = ET.fromstring(data)
+        AK = root.find('AK').attrib
+        desired_joint_correction = np.array([AK['A1'], AK['A2'], AK['A3'],
+                                             AK['A4'], AK['A5'], AK['A6']]).astype(np.float64)
+        IPOC = root.find('IPOC').text
+        return desired_joint_correction, int(IPOC)
 
 
 def main(args=None):
