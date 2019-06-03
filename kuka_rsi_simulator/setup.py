37ed5e13e28182d8fa2d5cb542a3b5f6e92d59ce
Lars Tingelstad <lars.tingelstad@ntnu.no>
2019-06-03 21:22:14 +0200

    Added kuka_rsi_simulator skeleton

---
+++ kuka_rsi_simulator/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+package_name = 'kuka_rsi_simulator'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Lars Tingelstad',
+    author_email='lars.tingelstad@ntnu.no',
+    maintainer='Lars Tingelstad',
+    maintainer_email='lars.tingelstad@ntnu.no',
+    keywords=['ROS', 'KUKA', 'RSI'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='KUKA RSI Simulator',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'kuka_rsi_simulator = kuka_rsi_simulator.kuka_rsi_simulator:main'
+        ],
+    },
+)
